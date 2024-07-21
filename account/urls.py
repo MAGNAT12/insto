3d3771cf31d7c007e31c1358576b9e024b677f1e
Magnat12 <rizamatmu@gmail.com>
2024-07-21 13:57:36 +0500

    ne rabotaet

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeDoneView, PasswordChangeView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView,
+)
+
 
 urlpatterns = [
     # path('login/', views.user_login, name='user_login'),
@@ -13,6 +23,20 @@ urlpatterns = [
     path('password-change/done/',
         PasswordChangeDoneView.as_view(),
         name='password_change_done'),
+
+        # url-адреса сброса пароля
+    path('password-reset/',
+        PasswordResetView.as_view(),
+        name='password_reset'),
+    path('password-reset/done/',
+        PasswordResetDoneView.as_view(),
+        name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/',
+        PasswordResetConfirmView.as_view(),
+        name='password_reset_confirm'),
+    path('password-reset/complete/',
+        PasswordResetCompleteView.as_view(),
+        name='password_reset_complete'),
 
     path('', views.dashboard, name='dashboard'),
 ]
